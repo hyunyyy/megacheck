ab3f1c9f4408fdeb43fe295e1c8c2625f76a84a4
Junyoung Hyun <49025879+hyunyyy@users.noreply.github.com>
2026-08-09 09:14:14 +0900

    Update sync.js

--- api/sync.js
+++ api/sync.js
@@ -13,8 +13,13 @@ function authorized(req) {
 }
 
 async function redis(command) {
-  const url = process.env.UPSTASH_REDIS_REST_URL;
-  const token = process.env.UPSTASH_REDIS_REST_TOKEN;
+  const url =
+  process.env.KV_REST_API_URL ||
+  process.env.UPSTASH_REDIS_REST_URL;
+
+  const token =
+  process.env.KV_REST_API_TOKEN ||
+  process.env.UPSTASH_REDIS_REST_TOKEN;
   if (!url || !token) throw new Error('Redis env vars are missing');
 
   const r = await fetch(url, {
